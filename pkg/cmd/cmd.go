f40b6a4d4a828a725d1813692b3a807eed0f7561
Oleg Sucharevich <olegs@codefresh.io>
2018-11-26 17:03:01 +0200

    add goreleaser yaml

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -14,7 +14,7 @@ func SetupCli() *cli.App {
 	app.Name = "Stevedore"
 	app.Description = "Integrate your connected clusters to your Codefresh account"
 	app.Email = "olegs@gmail.com"
-	app.Version = "1.1.0"
+	app.Version = "1.1.1"
 	setupCommands(app)
 	return app
 }
