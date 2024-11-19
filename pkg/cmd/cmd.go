aba5f71233dd28a561b0976658ac4b2afa1e84e5
Zhenya Tikhonov <zhenya.tikhonov@octopus.com>
2024-11-19 19:59:54 +0400

    ci: bump app version

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -14,7 +14,7 @@ func SetupCli() *cli.App {
 	app.Name = "Stevedore"
 	app.Description = "Integrate your connected clusters to your Codefresh account"
 	app.Email = "olegs@gmail.com"
-	app.Version = "1.6.1"
+	app.Version = "1.6.2"
 	setupCommands(app)
 	return app
 }
